a254c4a682c55ba15e4ffe7b6c04ed76803dc220
VladimirGolosin <vladimirgolosin1@gmail.com>
2022-12-06 16:35:39 +0100

    fix: change id from long to Long

--- uberapp/src/main/java/tim24/projekat/uberapp/model/Review.java
+++ uberapp/src/main/java/tim24/projekat/uberapp/model/Review.java
@@ -9,7 +9,7 @@ public class Review
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false, unique = true)
-    private long id;
+    private Long id;
     private int driverGrade;
     private int vehicleGrade;
     private String comment;
