b1667fc8fdb69274c86b346f3b818d0de6113254
REX1389 <rrvlelcc@gmail.com>
2022-12-07 12:45:20 +0100

    Creating repositories #1
    Created the Panic, Ride and Vehicle Repo.

--- uberapp/src/main/java/tim24/projekat/uberapp/repo/RideRepository.java
+++ uberapp/src/main/java/tim24/projekat/uberapp/repo/RideRepository.java
@@ -3,7 +3,9 @@ package tim24.projekat.uberapp.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tim24.projekat.uberapp.model.Ride;
 
+import java.util.Optional;
+
 public interface RideRepository extends JpaRepository<Ride, Long>
 {
-    
+    Optional<Ride> findRideById(Long id);
 }
---
+++ uberapp/src/main/java/tim24/projekat/uberapp/repo/VehicleRepository.java
@@ -0,0 +1,9 @@
+package tim24.projekat.uberapp.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tim24.projekat.uberapp.model.Vehicle;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long>
+{
+
+}
