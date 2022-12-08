3d9af2a7cfc30df9b316661c773373ebb774a639
REX1389 <rrvlelcc@gmail.com>
2022-12-08 22:47:58 +0100

    Some additional work on the panic DTO.

--- uberapp/src/main/java/tim24/projekat/uberapp/DTO/PanicDTO.java
+++ uberapp/src/main/java/tim24/projekat/uberapp/DTO/PanicDTO.java
@@ -2,5 +2,19 @@ package tim24.projekat.uberapp.DTO;
 
 public class PanicDTO
 {
-    
+    Long id;
+    UserRef user;
+    RideDTO ride;
+    String time;
+    String reason;
+
+    public PanicDTO()
+    {
+        super();
+        this.id = 0L;
+        this.user = new UserRef(0L, "d@gmail.com");
+        this.ride = new RideDTO();
+        this.time = "fafa";
+        this.reason = "gugu";
+    }
 }
