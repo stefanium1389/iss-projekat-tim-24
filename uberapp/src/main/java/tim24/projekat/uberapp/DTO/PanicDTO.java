34be4f6edfee20d9899c942cae8373d84b12d2ad
REX1389 <rrvlelcc@gmail.com>
2022-12-10 15:38:45 +0100

    Adding getters and setters to PanicDTO

--- uberapp/src/main/java/tim24/projekat/uberapp/DTO/PanicDTO.java
+++ uberapp/src/main/java/tim24/projekat/uberapp/DTO/PanicDTO.java
@@ -17,4 +17,54 @@ public class PanicDTO
         this.time = "fafa";
         this.reason = "gugu";*/
     }
+
+    public Long getId()
+    {
+        return this.id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public UserRef getUser()
+    {
+        return this.user;
+    }
+
+    public void set(UserRef user)
+    {
+        this.user = user;
+    }
+
+    public RideDTO getRide()
+    {
+        return this.ride;
+    }
+
+    public void set(RideDTO ride)
+    {
+        this.ride = ride;
+    }
+
+    public String getTime()
+    {
+        return this.time;
+    }
+
+    public void setTime(String time)
+    {
+        this.time = time;
+    }
+
+    public String getReason()
+    {
+        return this.reason;
+    }
+
+    public void setReason(String reason)
+    {
+        this.reason = reason;
+    }
 }
