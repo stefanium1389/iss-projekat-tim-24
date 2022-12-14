e93ed9b1e41eadc167833bf0de816daa3290c4c5
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2022-12-14 19:18:10 +0100

    All repos created

---
+++ uberapp/src/main/java/tim24/projekat/uberapp/repo/LocationRepository.java
@@ -0,0 +1,10 @@
+package tim24.projekat.uberapp.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tim24.projekat.uberapp.model.Location;
+
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+
+}
--- uberapp/src/main/java/tim24/projekat/uberapp/repo/VehicleDetailsRepository.java
+++ uberapp/src/main/java/tim24/projekat/uberapp/repo/VehicleDetailsRepository.java
@@ -1,10 +1,7 @@
 package tim24.projekat.uberapp.repo;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import tim24.projekat.uberapp.model.User;
 import tim24.projekat.uberapp.model.VehicleDetails;
 
 
