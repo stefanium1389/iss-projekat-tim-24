0c020f841fce6cc0e1d102f1c4ba2d3cd2e052f1
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-01-19 00:21:05 +0100

    vehicle controller

--- uberapp/src/main/java/tim24/projekat/uberapp/controller/VehicleController.java
+++ uberapp/src/main/java/tim24/projekat/uberapp/controller/VehicleController.java
@@ -3,15 +3,17 @@ package tim24.projekat.uberapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import tim24.projekat.uberapp.DTO.ErrorDTO;
-import tim24.projekat.uberapp.DTO.PasswordResetRequestDTO;
+import tim24.projekat.uberapp.DTO.GeoCoordinateDTO;
 import tim24.projekat.uberapp.DTO.SuccessDTO;
 import tim24.projekat.uberapp.DTO.VehicleDTO;
-import tim24.projekat.uberapp.exception.ConditionNotMetException;
 import tim24.projekat.uberapp.exception.ObjectNotFoundException;
-import tim24.projekat.uberapp.service.PasswordResetService;
 import tim24.projekat.uberapp.service.VehicleService;
 
 @RestController
@@ -22,10 +24,18 @@ public class VehicleController
 	private VehicleService vehicleService;
 	
     @PutMapping("/{id}/location")
-    public ResponseEntity<VehicleDTO> putVehicleLocation(@PathVariable("id") Long id)
+    public ResponseEntity<?> putVehicleLocation(@PathVariable("id") Long id,@RequestBody GeoCoordinateDTO dto)
     {
-    	vehicleService.putVehicleLocation(id); //izmeni ovo
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    	try {
+    		vehicleService.putVehicleLocation(id,dto); //izmeni ovo
+    		SuccessDTO s = new SuccessDTO("Coordinates successfully updated");
+            return new ResponseEntity<SuccessDTO>(s,HttpStatus.NO_CONTENT);
+    	}
+    	catch(ObjectNotFoundException e) {
+    		ErrorDTO err = new ErrorDTO(e.getMessage());
+    		return new ResponseEntity<ErrorDTO>(err,HttpStatus.NOT_FOUND);
+    	}
+    	
     }
     
 }
