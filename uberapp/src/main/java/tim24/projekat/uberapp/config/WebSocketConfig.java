427ea8fad2f7539914208c17b7835757370b96b9
REX1389 <rrvlelcc@gmail.com>
2023-01-19 23:23:35 +0100

    Adding the web socket controller

--- uberapp/src/main/java/tim24/projekat/uberapp/config/WebSocketConfig.java
+++ uberapp/src/main/java/tim24/projekat/uberapp/config/WebSocketConfig.java
@@ -16,8 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
      */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socket") // Definisemo endpoint koji ce klijenti koristiti da se povezu sa serverom.
-                // U ovom slucaju, URL za konekciju ce biti http://localhost:8080/socket/
+        registry.addEndpoint("/socket-connect") // Definisemo endpoint koji ce klijenti koristiti da se povezu sa serverom.
+                // U ovom slucaju, URL za konekciju ce biti http://localhost:8080/socket-connect/
                 .setAllowedOrigins("*") // Dozvoljavamo serveru da prima zahteve bilo kog porekla
                 .withSockJS(); // Koristi se SockJS: https://github.com/sockjs/sockjs-protocol
     }
@@ -28,10 +28,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
      */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/socket-subscriber") // Prefiks koji koji se koristi za mapiranje svih poruka.
+        registry.setApplicationDestinationPrefixes("/socket") // Prefiks koji koji se koristi za mapiranje svih poruka.
                 // Klijenti moraju da ga navedu kada salju poruku serveru.
-                // Svaki URL bi pocinjao ovako: http://localhost:8080/socket-subscriber/…/…
-                .enableSimpleBroker("/socket-publisher"); // Definisanje topic-a (ruta) na koje klijenti mogu da se pretplate.
+                // Svaki URL bi pocinjao ovako: http://localhost:8080/socket/…/…
+                .enableSimpleBroker("/notification", "/map"); // Definisanje topic-a (ruta) na koje klijenti mogu da se pretplate.
         // SimpleBroker cuva poruke u memoriji i salje ih klijentima na definisane topic-e.
         // Server kada salje poruke, salje ih na rute koje su ovde definisane, a klijenti cekaju na poruke.
         // Vise ruta odvajamo zarezom, npr. enableSimpleBroker("/ruta1", "/ruta2");
---
+++ uberapp/src/main/java/tim24/projekat/uberapp/controller/WebSocketController.java
@@ -0,0 +1,60 @@
+package tim24.projekat.uberapp.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+import tim24.projekat.uberapp.DTO.DTOList;
+import tim24.projekat.uberapp.DTO.VehicleDTO;
+import tim24.projekat.uberapp.model.Vehicle;
+import tim24.projekat.uberapp.repo.VehicleRepository;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class WebSocketController
+{
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    @Autowired
+    private VehicleRepository vehicleRepo;
+
+    @MessageMapping("/send/notification")
+    public Map<String, String> broadcastNotification(String message)
+    {
+        Map<String, String> messageConverted = parseMessage(message);
+        this.simpMessagingTemplate.convertAndSend("/notification/" + messageConverted.get("userId"), messageConverted);
+        return messageConverted;
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void broadcastMap()
+    {
+        List<Vehicle> vehicles = vehicleRepo.findAll();
+        DTOList list = new DTOList();
+        for(Vehicle v: vehicles)
+            list.add(new VehicleDTO(v));
+        this.simpMessagingTemplate.convertAndSend("/map", list);
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, String> parseMessage(String message)
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> retVal;
+
+        try {
+            retVal = mapper.readValue(message, Map.class); // parsiranje JSON stringa
+        } catch (IOException e) {
+            retVal = null;
+        }
+
+        return retVal;
+    }
+}
