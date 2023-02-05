e16e6acdb44fc83d2a4b0adf849448d65e06ef99
VladimirGolosin <vladimirgolosin1@gmail.com>
2023-02-05 13:55:27 +0100

    feat: get locations of drivers for map

--- uberapp/src/main/java/tim24/projekat/uberapp/service/VehicleService.java
+++ uberapp/src/main/java/tim24/projekat/uberapp/service/VehicleService.java
@@ -1,26 +1,40 @@
 package tim24.projekat.uberapp.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tim24.projekat.uberapp.DTO.DTOList;
 import tim24.projekat.uberapp.DTO.GeoCoordinateDTO;
+import tim24.projekat.uberapp.DTO.MapDetailDTO;
 import tim24.projekat.uberapp.DTO.VehicleChangeDTO;
 import tim24.projekat.uberapp.DTO.VehicleDTO;
 import tim24.projekat.uberapp.exception.ObjectNotFoundException;
 import tim24.projekat.uberapp.model.Location;
+import tim24.projekat.uberapp.model.Ride;
 import tim24.projekat.uberapp.model.Vehicle;
 import tim24.projekat.uberapp.model.VehicleType;
+import tim24.projekat.uberapp.model.WorkingHour;
 import tim24.projekat.uberapp.repo.LocationRepository;
+import tim24.projekat.uberapp.repo.RideRepository;
 import tim24.projekat.uberapp.repo.VehicleRepository;
 import tim24.projekat.uberapp.repo.VehicleTypeRepository;
+import tim24.projekat.uberapp.repo.WorkingHourRepo;
 
 @Service
 public class VehicleService
 {
 	@Autowired
 	private VehicleRepository vehicleRepo;
+	
+	@Autowired
+	private RideRepository rideRepo;
+	
+	@Autowired
+	private WorkingHourRepo whRepo;
+	
 	@Autowired
 	private VehicleTypeRepository vehicleTypeRepo;
 	@Autowired
@@ -77,6 +91,41 @@ public class VehicleService
 		vehicleRepo.save(v);
 		vehicleRepo.flush();
 		return new VehicleDTO(v);
+	}
+
+	public DTOList<MapDetailDTO> getDetailsForMap() {
+		
+		DTOList<MapDetailDTO> dtoList = new DTOList<MapDetailDTO>();
+		List<Vehicle> vehicles = vehicleRepo.findAll();
+		if (vehicles.isEmpty()) 
+		{
+			return dtoList;
+		}
+
+		for (Vehicle v : vehicles) 
+		{
+			if (v.getLocation()==null) continue;
+			
+			String status = "OFFLINE";
+			Optional<Ride> ride = rideRepo.findActiveRideByDriverId(v.getDriver().getId());
+			Optional<WorkingHour> wh = whRepo.findLastWorkingHourByDriverId(v.getDriver().getId());
+			if (ride.isPresent()) 
+			{
+				status = "RIDE";
+			}
+			
+			else if (wh.isPresent()) 
+			{
+				status = "ONLINE";
+			}
+			
+			
+			MapDetailDTO mdd = new MapDetailDTO(v.getLocation().getLatitude(), v.getLocation().getLongitude(), status);
+			dtoList.add(mdd);
+		}
+		
+		
+		return dtoList;
 	}
 
 }
