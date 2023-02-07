aef579e78db5c1ea465f555db7d3407917b02486
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-02-07 02:50:32 +0100

    joooj

--- uberapp/src/test/java/tim24/projekat/uberapp/controller/PostRideControllerTests.java
+++ uberapp/src/test/java/tim24/projekat/uberapp/controller/PostRideControllerTests.java
@@ -1,16 +1,27 @@
-package tim24.projekat.uberapp;
+package tim24.projekat.uberapp.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-
+import tim24.projekat.uberapp.DTO.RideDTO;
 import tim24.projekat.uberapp.security.JwtTokenUtil;
 
 
@@ -18,16 +29,19 @@ import tim24.projekat.uberapp.security.JwtTokenUtil;
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-public class RideControllerTests {
+@Sql("classpath:testdata.sql")
+public class PostRideControllerTests {
 	
 	@Autowired
 	MockMvc mockMvc;
 	
 	@Autowired
 	JwtTokenUtil jwtTokenUtil;
-	
+
 	@Test
     public void postRide_withoutValidAuthorization_returnsForbidden() throws Exception {
+		
+		System.out.println("1.");
      
 		String requestJson = "{\r\n"
 				+ "  \"locations\": [\r\n"
@@ -62,8 +76,50 @@ public class RideControllerTests {
 		
 	}
 	@Test
+    public void postRide_NoValidDriver_returnsBadRequest() throws Exception {
+		System.out.println("2.");
+
+		String requestJson = "{\r\n"
+				+ "  \"locations\": [\r\n"
+				+ "    {\r\n"
+				+ "      \"departure\": {\r\n"
+				+ "        \"address\": \"Bulevar oslobodjenja 46\",\r\n"
+				+ "        \"latitude\": 45.267136,\r\n"
+				+ "        \"longitude\": 19.833549\r\n"
+				+ "      },\r\n"
+				+ "      \"destination\": {\r\n"
+				+ "        \"address\": \"Bulevar oslobodjenja 46\",\r\n"
+				+ "        \"latitude\": 45.267136,\r\n"
+				+ "        \"longitude\": 19.833549\r\n"
+				+ "      }\r\n"
+				+ "    }\r\n"
+				+ "  ],\r\n"
+				+ "  \"passengers\": [\r\n"
+				+ "    {\r\n"
+				+ "      \"id\": 2,\r\n"
+				+ "      \"email\": \"stefanium@mail.com\"\r\n"
+				+ "    }\r\n"
+				+ "  ],\r\n"
+				+ "  \"vehicleType\": \"STANDARD\",\r\n"
+				+ "  \"babyTransport\": true,\r\n"					/// nemamo nikog ko vozi bebe, pa nece naci vozaca
+				+ "  \"petTransport\": true,\r\n"
+				+ "  \"scheduledTime\": null\r\n"
+				+ "}";
+		
+        String jwt = jwtTokenUtil.generateToken("stefanium@mail.com");
+        String status = "ACCEPTED";
+        
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/ride") //poruci voznju
+        		.header("Authorization", "Bearer " + jwt)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest()); 
+		
+	}
+	@Test
     public void postRide_HappyFlow() throws Exception {
-     
+		System.out.println("3.");
+
 		String requestJson = "{\r\n"
 				+ "  \"locations\": [\r\n"
 				+ "    {\r\n"
@@ -101,9 +157,11 @@ public class RideControllerTests {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(status));
 		
 	}
+	
 	@Test
-    public void postRide_WhileHavsPendingRide_returnsBadRequest() throws Exception {
-     
+    public void postRide_WhileHasPendingRide_returnsBadRequest() throws Exception {
+		System.out.println("4.");
+
 		String requestJson = "{\r\n"
 				+ "  \"locations\": [\r\n"
 				+ "    {\r\n"
@@ -141,7 +199,46 @@ public class RideControllerTests {
                 .andExpect(MockMvcResultMatchers.status().isBadRequest()); 
 		
 	}
+	
+	@Test
+    public void getDriverActiveRide_HappyFlow() throws Exception {
+		System.out.println("5.");
 
+		String jwt = jwtTokenUtil.generateToken("vlafa@mail.com");
+
+		 mockMvc.perform(MockMvcRequestBuilders.get("/api/ride/driver/3/active") 
+	        		.header("Authorization", "Bearer " + jwt))
+	                .andExpect(MockMvcResultMatchers.status().isOk()); 
+		
+    }
+	@Test
+    public void getDriverActiveRide_withoutAuthorization_returnsForbidden() throws Exception {
+		System.out.println("6.");
+
+		 mockMvc.perform(MockMvcRequestBuilders.get("/api/ride/driver/2/active"))
+	                .andExpect(MockMvcResultMatchers.status().isForbidden()); 
+		
+    }
+	
+	@Test
+    public void getPassengerActiveRide_HappyFlow() throws Exception {
+		System.out.println("7.");
+
+		String jwt = jwtTokenUtil.generateToken("stefanium@mail.com");
+		 mockMvc.perform(MockMvcRequestBuilders.get("/api/ride/passenger/2/active") 
+	        		.header("Authorization", "Bearer " + jwt))
+	                .andExpect(MockMvcResultMatchers.status().isOk()); 
+		
+    }
+	@Test
+    public void getPassengerActiveRide_withoutAuthorization_returnsForbidden() throws Exception {
+		System.out.println("8.");
+
+
+		 mockMvc.perform(MockMvcRequestBuilders.get("/api/ride/passenger/2/active"))
+	                .andExpect(MockMvcResultMatchers.status().isForbidden()); 
+		
+    }
 }
 
 
